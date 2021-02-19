3b712141b939aec33e497508dc53f5ed83672f46
AJSolis01 <57882033+AJSolis01@users.noreply.github.com>
2021-02-19 10:33:57 -0500

    Add files via upload

---
+++ Monthly Maintenance/Maint2/run.ps1
@@ -0,0 +1,63 @@
+using namespace System.Net
+# Write Admin Rights
+
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
+{  
+  $arguments = "& '" +$myinvocation.mycommand.definition + "'"
+  Start-Process powershell -Verb runAs -ArgumentList $arguments
+  Break
+}
+
+# Write to the Azure Functions log stream.
+Write-Host "PowerShell HTTP trigger function processed a request."
+
+# Interact with query parameters or the body of the request.
+param($MyFirstInputBinding, $MySecondInputBinding, $TriggerMetadata)
+
+
+# Install AutomateAPI
+If ($null -eq (Get-Module -ListAvailable -Name AutomateAPI)[0]) { Install-Module -Name AutomateAPI -Force } else { Import-Module -Name AutomateAPI }
+
+# Install PoshRSJob
+If ($null -eq (Get-Module -ListAvailable -Name PoshRSJob)[0]) { Install-Module -Name PoshRSJob -Force -AllowClobber } else { Import-Module -Name PoshRSJob }
+
+
+Import-Module PoshRSJob 
+Import-Module AutomateAPI
+Connect-AutomateAPI 
+$link = "<a href='https://atgfw.hostedrmm.com/'>$ReqID</a>"
+
+Connect-ControlAPI
+$link = "<a href='https://atgfw.hostedrmm.com:8040/'>$ReqID</a>"
+
+
+
+Get-AutomateComputer -Online $False | Compare-AutomateControlStatus
+
+Get-AutomateComputer -Online $True | Compare-AutomateControlStatus
+
+
+Get-AutomateComputer -Online $False | Compare-AutomateControlStatus | Repair-AutomateAgent -Action Check -Confirm:$false
+
+Get-AutomateComputer -Online $False | Compare-AutomateControlStatus | Repair-AutomateAgent -Action Restart -Confirm:$false
+
+Get-AutomateComputer -Online $False | Compare-AutomateControlStatus | Repair-AutomateAgent -Action Update -Confirm:$false
+
+Get-AutomateComputer -Online $False | Compare-AutomateControlStatus | Repair-AutomateAgent -Action Reinstall -Confirm:$false
+
+
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = [HttpStatusCode]::OK
+    Body = $body
+})
+
+Pause 
+
+
+
